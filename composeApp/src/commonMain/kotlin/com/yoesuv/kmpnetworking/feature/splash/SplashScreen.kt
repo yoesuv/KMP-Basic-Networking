c3668c1ea36ecf28ec728a7e1e78422ef61470ba
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2025-07-22 21:06:26 +0700

    Adds splash screen and dependencies
    Sets up a basic splash screen for initial app loading.
    Also configures necessary dependencies such as kotlinx-serialization-json and navigation-compose.

---
+++ composeApp/src/commonMain/kotlin/com/yoesuv/kmpnetworking/feature/splash/SplashScreen.kt
@@ -0,0 +1,39 @@
+package com.yoesuv.kmpnetworking.feature.splash
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+@Composable
+fun SplashScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize()
+            .background(Color.White),
+        contentAlignment = Alignment.Center,
+    ) {
+        Text(
+            "KMP Networking", style = TextStyle(
+                fontSize = 24.sp,
+                fontWeight = FontWeight.SemiBold,
+            )
+        )
+    }
+}
+
+@Preview
+@Composable
+fun PreviewSplashScreen() {
+    MaterialTheme {
+        SplashScreen()
+    }
+}
